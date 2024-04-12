79c1f896bc11bb6507f4d320c5c83aebca85784c
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-04-12 10:47:37 +0530

    build complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import PropertyCard from "@/components/shared/cards/property-card"
-import Header from "@/components/shared/navigation/Header"
-import SearchArea from "@/components/shared/search-area/SearchArea"
-import SortIcon from '@/assets/sort.png'
-import { useEffect, useState } from "react"
-import ShimmerPropertyCard from "@/components/shared/cards/shimmer-property-card"
-import { mockData } from "./utils/mockData"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import FilterCard from "./components/shared/cards/filter-card"
+import PropertyCard from "@/components/shared/cards/property-card";
+import Header from "@/components/shared/navigation/Header";
+import SearchArea from "@/components/shared/search-area/SearchArea";
+import SortIcon from '@/assets/sort.png';
+import { useEffect, useState } from "react";
+import ShimmerPropertyCard from "@/components/shared/cards/shimmer-property-card";
+import { mockData } from "./utils/mockData";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import FilterCard from "./components/shared/cards/filter-card";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -14,6 +14,7 @@ function App() {
   const [sortBy, setSortBy] = useState<string>("");
   const [sortOrder, setSortOrder] = useState<string>("");
   const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const [filteredData, setFilteredData] = useState(mockData);
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -29,13 +30,16 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  const filteredData1 = mockData.filter(property =>
-    property.propertyName.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
+  useEffect(() => {
+    // Update filteredData whenever searchQuery changes
+    const newData = mockData.filter(property =>
+      property.propertyName.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredData(newData);
+  }, [searchQuery]);
 
   // Sorting function based on the current sorting criteria and order
-  const sortedData = [...filteredData1].sort((a, b) => {
+  const sortedData = [...filteredData].sort((a, b) => {
     switch (sortBy) {
       case "price":
         return sortOrder === "asc" ? Number(a.propertyPrice) - Number(b.propertyPrice) : Number(b.propertyPrice) - Number(a.propertyPrice);
@@ -53,40 +57,40 @@ function App() {
 
   return (
     <div className="pt-5 space-y-4">
-     <Header />
-     <main className="bg-[#F6F8FA]">
-      <SearchArea setSearchQuery={setSearchQuery} />
-      <div className="flex flex-col md:flex-row mx-auto max-w-7xl space-x-5 mt-6">
-        <div className="px-5 mb-2 md:px-0 md:mb-0">
-          <FilterCard />
-        </div>
-      <div className="space-y-6 px-5 mt-5 md:mt-0 md:px-0">
-        <div className="flex justify-end">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <button className="bg-white flex items-center outfitMedium space-x-2 px-4 py-2 cursor-pointer" onClick={toggleDropdown}>
-                <img src={SortIcon} className="w-5" />
-                <h1>Sort By</h1>
-              </button>
-            </DropdownMenuTrigger>
-            {dropdownOpen && (
-            <DropdownMenuContent className="w-[320px] bg-white space-y-2 -mt-11">
-              <button className="bg-white flex items-center outfitMedium space-x-2 px-4 py-2 cursor-pointer">
-                <img src={SortIcon} className="w-5" />
-                <h1>Sort By</h1>
-              </button>
-              <div>
-                <DropdownMenuLabel>Property Price</DropdownMenuLabel>
-                <DropdownMenuGroup>
-                  <DropdownMenuItem onClick={() => { setSortBy("price"); setSortOrder("desc"); }}>
-                    High to Low
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => { setSortBy("price"); setSortOrder("asc"); }}>
-                    Low to High
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </div>
-              <DropdownMenuSeparator />
+      <Header />
+      <main className="bg-[#F6F8FA]">
+        <SearchArea setSearchQuery={setSearchQuery} />
+        <div className="flex flex-col md:flex-row mx-auto max-w-7xl space-x-5 mt-6">
+          <div className="px-5 mb-2 md:px-0 md:mb-0">
+            <FilterCard setFilteredData={setFilteredData} setLoading={setLoading}/>
+          </div>
+          <div className="space-y-6 px-5 mt-5 md:mt-0 md:px-0">
+            <div className="flex justify-end">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <button className="bg-white flex items-center outfitMedium space-x-2 px-4 py-2 cursor-pointer" onClick={toggleDropdown}>
+                    <img src={SortIcon} className="w-5" />
+                    <h1>Sort By</h1>
+                  </button>
+                </DropdownMenuTrigger>
+                {dropdownOpen && (
+                  <DropdownMenuContent className="w-[320px] bg-white space-y-2 -mt-11">
+                    <button className="bg-white flex items-center outfitMedium space-x-2 px-4 py-2 cursor-pointer">
+                      <img src={SortIcon} className="w-5" />
+                      <h1>Sort By</h1>
+                    </button>
+                    <div>
+                      <DropdownMenuLabel>Property Price</DropdownMenuLabel>
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem onClick={() => { setSortBy("price"); setSortOrder("desc"); }}>
+                          High to Low
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => { setSortBy("price"); setSortOrder("asc"); }}>
+                          Low to High
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                    </div>
+                    <DropdownMenuSeparator />
               <div>
                 <DropdownMenuLabel>Plot Area</DropdownMenuLabel>
                 <DropdownMenuGroup>
@@ -99,61 +103,61 @@ function App() {
                 </DropdownMenuGroup>
               </div>
               <DropdownMenuSeparator />
-              <div>
-                <DropdownMenuLabel>Ratings</DropdownMenuLabel>
-                <DropdownMenuGroup>
-                  <DropdownMenuItem onClick={() => { setSortBy("rating"); setSortOrder("desc"); }}>
-                    High to Low
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => { setSortBy("rating"); setSortOrder("asc"); }}>
-                    Low to High
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
+                    <div>
+                      <DropdownMenuLabel>Ratings</DropdownMenuLabel>
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem onClick={() => { setSortBy("rating"); setSortOrder("desc"); }}>
+                          High to Low
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => { setSortBy("rating"); setSortOrder("asc"); }}>
+                          Low to High
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                    </div>
+                    <DropdownMenuSeparator />
+                    <div>
+                      <DropdownMenuLabel>Alphabetical</DropdownMenuLabel>
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem onClick={() => { setSortBy("alphabetical"); setSortOrder("asc"); }}>
+                          A to Z
+                        </DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => { setSortBy("alphabetical"); setSortOrder("desc"); }}>
+                          Z to A
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                    </div>
+                  </DropdownMenuContent>
+                )}
+              </DropdownMenu>
+            </div>
+            {loading ? (
+              <div className="space-y-4">
+                <ShimmerPropertyCard />
+                <ShimmerPropertyCard />
+                <ShimmerPropertyCard />
               </div>
-              <DropdownMenuSeparator />
-              <div>
-                    <DropdownMenuLabel>Alphabetical</DropdownMenuLabel>
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem onClick={() => { setSortBy("alphabetical"); setSortOrder("asc"); }}>
-                        A to Z
-                      </DropdownMenuItem>
-                      <DropdownMenuItem onClick={() => { setSortBy("alphabetical"); setSortOrder("desc"); }}>
-                        Z to A
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                  </div>
-            </DropdownMenuContent>
+            ) : (
+              <div className="space-y-5">
+                {sortedData.map((property) => (
+                  <PropertyCard
+                    key={property.id}
+                    propertyImage={property.propertyImage}
+                    propertyName={property.propertyName}
+                    propertyPrice={property.propertyPrice}
+                    propertyDescription={property.propertyDescription}
+                    plotArea={property.plotArea}
+                    rooms={property.rooms}
+                    ratings={property.ratings}
+                    bookingAmt={property.bookingAmt}
+                  />
+                ))}
+              </div>
             )}
-          </DropdownMenu>
-        </div>
-        {loading ? (
-         <div className="space-y-4">
-           <ShimmerPropertyCard />
-           <ShimmerPropertyCard />
-           <ShimmerPropertyCard />
-         </div>
-        ) : (
-          <div className="space-y-5">
-            {sortedData.map((property) => (
-              <PropertyCard
-                key={property.id}
-                propertyImage={property.propertyImage}
-                propertyName={property.propertyName}
-                propertyPrice={property.propertyPrice}
-                propertyDescription={property.propertyDescription}
-                plotArea={property.plotArea}
-                rooms={property.rooms}
-                ratings={property.ratings}
-                bookingAmt={property.bookingAmt}
-              />
-            ))}
           </div>
-        )}
-      </div>
-     </div>
-     </main>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/shared/cards/filter-card.tsx
+++ src/components/shared/cards/filter-card.tsx
@@ -1,171 +1,280 @@
-import { Card } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { SearchIcon } from "lucide-react"
-import FilterIcon from '@/assets/filter.png'
-import PriceIcon from '@/assets/moneys.png'
-import RatingIcon from '@/assets/staroutline.png'
-import { Slider } from "@mui/material"
-import { useEffect, useState } from "react"
-import { mockData } from "@/utils/mockData"
+import React, { useState, useEffect } from "react";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+import FilterIcon from "@/assets/filter.png";
+import PriceIcon from "@/assets/moneys.png";
+import RatingIcon from "@/assets/staroutline.png";
+import { Slider, styled } from "@mui/material";
+import { mockData } from "@/utils/mockData";
 
 interface RatingProps {
-    value: number;
-    checked: boolean;
-    onChange: (value: number) => void;
+  value: number;
+  checked: boolean;
+  onChange: (value: number) => void;
 }
 
 interface PropertyType {
-    id: number;
-    propertyImage: string;
-    propertyName: string;
-    propertyPrice: number;
-    propertyDescription: string;
-    plotArea: string;
-    rooms: string;
-    ratings: string;
-    bookingAmt: string;
-  }
-
-function valuetext(value: number) {
-    return `${value}°C`;
-  }
-  
-  const Rating: React.FC<RatingProps> = ({ value, checked, onChange }) => (
-    <div className="flex gap-4 mt-4 text-base leading-6 text-neutral-500">
-      <div
-        className={`shrink-0 my-auto w-4 h-4 rounded-full border border-solid ${
-          checked ? "border-4 border-indigo-600" : "border-neutral-300"
-        } stroke-[1px]`}
-        onClick={() => onChange(value)}
-      />
-      <div className="outfitRegular">{value} +</div>
-    </div>
-  );
-
-const FilterCard = () => {
-    const [loading, setLoading] = useState(true);
-    const [value, setValue] = useState<number[]>([0, 100000000]);
-    const [selectedRatings, setSelectedRatings] = useState<number[]>([]);
-    const [priceFilter] = useState<number[]>([0, 100000000]);
-    const [searchQuery, setSearchQuery] = useState<string>('');
-    const [ratingFilter] = useState<number[]>([]);
-    const [filterApplied, setFilterApplied] = useState<boolean>(false);
-    const [filteredData, setFilteredData] = useState<PropertyType[]>([]);
-
-    console.log(loading,filteredData)
-
-    const handleChange = (_event: Event, newValue: number | number[]) => {
-        setValue(newValue as number[]);
-    };
-
-    const handleRatingChange = (rating: number) => {
-        if (selectedRatings.includes(rating)) {
-          setSelectedRatings(selectedRatings.filter((r) => r !== rating));
-        } else {
-          setSelectedRatings([...selectedRatings, rating]);
-        }
-      };
-    
-      const ratings = [
-        { value: 4.5, checked: selectedRatings.includes(4.5) },
-        { value: 4, checked: selectedRatings.includes(4) },
-        { value: 3.5, checked: selectedRatings.includes(3.5) },
-        { value: 3, checked: selectedRatings.includes(3) },
-      ];
-
-      const applyFilters = () => {
-        // Filter data based on selected options
-        let newData = mockData.filter(property => {
-          // Apply price filter
-          const priceInRange = Number(property.propertyPrice) >= priceFilter[0] && Number(property.propertyPrice) <= priceFilter[1];
-          // Apply rating filter if ratings are selected
-          const meetsRating = ratingFilter.length === 0 || ratingFilter.includes(parseFloat(property.ratings));
-          // Apply search query filter
-          const matchesSearch = property.propertyName.toLowerCase().includes(searchQuery.toLowerCase());
-      
-          return priceInRange && meetsRating && matchesSearch;
-        });
-      
-        // Update filtered data and set filterApplied to true
-        setFilteredData(newData);
-        setFilterApplied(true);
-      };
-    
-      useEffect(() => {
-        // Apply filters only when filter is applied
-        if (filterApplied) {
-          setLoading(true); // Show shimmer effect
-          // Simulate a delay to showcase the shimmer effect
-          const timer = setTimeout(() => {
-            setLoading(false);
-          }, 2000);
-          // Clear the timer when component unmounts
-          return () => clearTimeout(timer);
-        }
-      }, [filterApplied]);
-  return (
-    <Card className="md:w-[320px]">
-            <div className="space-y-12 p-6">
-              <div className="space-y-3">
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center space-x-1">
-                    <img src={FilterIcon} className="w-5" />
-                    <h1 className="text-[#4D4D4D] text-lg outfitMedium">Filter</h1>
-                  </div>
-                  <div>
-                    <h1 className="text-[#533FDB] text-sm cursor-pointer outfitRegular">Clear All</h1>
-                  </div>
-                </div>
-                <div className="relative text-[#1C1C1C66]">
-                  <Input
-                    type="text"
-                    placeholder="Search by 'Developer'"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    className="focus:outline-[#716AEA08] bg-[#F8F8F8] border outfitRegular bordersearch py-5 pl-10 lg:w-[272px]"
-                  />
-                  <SearchIcon className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-gray-400 lg:left-3" />
-                </div>
-              </div>
-              <div>
-                <div className="flex items-center space-x-1">
-                  <img src={PriceIcon} className="w-5" />
-                  <h1 className="text-[#1C1C1C] text-lg outfitMedium">Price</h1>
-                </div>
-                <div className="mt-2 w-auto max-w-[250px]">
-                  <div className="relative">
-                    <Slider
-                      getAriaLabel={() => 'Property Price Range'}
-                      value={value}
-                      className="ml-2"
-                      onChange={handleChange}
-                      valueLabelDisplay="auto"
-                      getAriaValueText={valuetext}
-                    />
-                  </div>
-                  <div className="flex items-center outfitRegular justify-between text-sm text-neutral-600">
-                    <div className="">₹ {value[0]}</div>
-                    <div className="">₹ {value}</div>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <div className="flex items-center space-x-1">
-                  <img src={RatingIcon} className="w-5" />
-                  <h1 className="text-[#1C1C1C] text-lg outfitMedium">Rating</h1>
-                </div>
-                <div>
-                {ratings.map((rating) => (
-                  <Rating key={rating.value} value={rating.value} checked={rating.checked} onChange={handleRatingChange}/>
-                ))}
-                </div>
-              </div>
-              <div>
-                <button onClick={applyFilters} className="bg-[#533FDB] text-white py-2 px-[116px] rounded">Apply</button>
-              </div>
-            </div>
-          </Card>
-  )
+  id: number;
+  propertyImage: string;
+  propertyName: string;
+  propertyPrice: number;
+  propertyDescription: string;
+  plotArea: string;
+  rooms: string;
+  ratings: string;
+  bookingAmt: string;
 }
 
-export default FilterCard
+interface FilterCardProps {
+  setFilteredData: React.Dispatch<React.SetStateAction<PropertyType[]>>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function valuetext(value: number) {
+  return `₹ ${value.toLocaleString(undefined, {
+    maximumFractionDigits: 2,
+  })} Crore`;
+}
+
+const Rating: React.FC<RatingProps> = ({ value, checked, onChange }) => (
+  <div className="flex gap-4 mt-4 text-base leading-6 text-neutral-500">
+    <div
+      className={`shrink-0 my-auto w-4 h-4 rounded-full border border-solid ${
+        checked ? "border-4 border-indigo-600" : "border-neutral-300"
+      } stroke-[1px]`}
+      onClick={() => onChange(value)}
+    />
+    <div className="outfitRegular">{value} +</div>
+  </div>
+);
+
+const FilterCard: React.FC<FilterCardProps> = ({ setFilteredData, setLoading }) => {
+  const [value, setValue] = useState<number[]>([0, 100000000]);
+  const [selectedRatings, setSelectedRatings] = useState<number[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [filterApplied, setFilterApplied] = useState<boolean>(false);
+  const [filtersChanged, setFiltersChanged] = useState<boolean>(false);
+  const [filters, setFilters] = useState<any>({
+    value: [0, 100000000],
+    selectedRatings: [],
+    searchQuery: "",
+  });
+
+  console.log(filters)
+
+  const handleChange = (_event: Event, newValue: number | number[]) => {
+    setValue(newValue as number[]);
+    setFilters({ ...filters, value: newValue });
+  };
+
+  const handleRatingChange = (rating: number) => {
+    setSelectedRatings((prevSelectedRatings) => {
+      if (prevSelectedRatings.includes(rating)) {
+        return prevSelectedRatings.filter((r) => r !== rating);
+      } else {
+        return [...prevSelectedRatings, rating];
+      }
+    });
+  };
+  
+  useEffect(() => {
+    setFilters((prevFilters: any) => ({
+      ...prevFilters,
+      selectedRatings: selectedRatings,
+    }));
+  }, [selectedRatings]);
+
+  useEffect(() => {
+    const isFiltersChanged =
+      JSON.stringify(filters) !== JSON.stringify({
+        value: [0, 100000000],
+        selectedRatings: [],
+        searchQuery: "",
+      });
+    setFiltersChanged(isFiltersChanged);
+  }, [filters]);
+  
+  const applyFilters = () => {  
+    // Set loading state to true to show shimmer
+    setLoading(true);
+  
+    // Simulate filtering delay
+    setTimeout(() => {
+      // Filter data based on selected options
+      const newData = mockData.filter(property => {
+        // Apply price filter
+        const priceInRange = property.propertyPrice >= value[0] * 10000000 && property.propertyPrice <= value[1] * 10000000;
+        // Convert property.ratings to a number for comparison
+        const propertyRating = parseFloat(property.ratings);
+        // Apply rating filter if ratings are selected
+        const meetsRating = selectedRatings.length === 0 || selectedRatings.includes(propertyRating);
+        // Apply search query filter
+        const matchesSearch = property.propertyName.toLowerCase().includes(searchQuery.toLowerCase());
+  
+        return priceInRange && meetsRating && matchesSearch;
+      });
+  
+      console.log('Filtered Data:', newData);
+  
+      // Update filtered data and set filterApplied to true
+      setFilteredData(newData);
+      setFilterApplied(true);
+  
+      // Reset loading state after filtering is complete
+      setLoading(false);
+    }, 2000); // Simulated delay time
+  };
+  
+
+  const PriceRangeSlider = styled(Slider)(({ theme }) => ({
+    color: "#716AEA",
+    height: 3,
+    padding: "12px 0",
+    "& .MuiSlider-thumb": {
+      height: 27,
+      width: 27,
+      backgroundColor: "#716AEA",
+      border: "1px solid currentColor",
+      "&:hover": {
+        boxShadow: "0 0 0 8px rgba(58, 133, 137, 0.16)",
+      },
+      "& .pricerange-bar": {
+        height: 9,
+        width: 1,
+        backgroundColor: "currentColor",
+        marginLeft: 1,
+        marginRight: 1,
+      },
+    },
+    "& .MuiSlider-track": {
+      height: 3,
+    },
+    "& .MuiSlider-rail": {
+      color: theme.palette.mode === "dark" ? "#bfbfbf" : "#d8d8d8",
+      opacity: theme.palette.mode === "dark" ? undefined : 1,
+      height: 3,
+    },
+  }));
+
+  const formatPrice = (price: number): string => {
+    if (price >= 10000000) {
+      const crorePrice = price / 10000000;
+      return `${crorePrice.toFixed(1)} Cr`;
+    }
+    const lakhPrice = price / 100000;
+    return `${lakhPrice.toFixed(0)} Lakhs`;
+  };
+
+  useEffect(() => {
+    if (filterApplied) {
+      setLoading(true);
+      const timer = setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [filterApplied]);
+
+  return (
+    <Card className="md:w-[320px]">
+      <div className="space-y-9 p-6">
+        <div className="space-y-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center space-x-1">
+              <img src={FilterIcon} className="w-5" />
+              <h1 className="text-[#4D4D4D] text-lg outfitMedium">Filter</h1>
+            </div>
+            <div>
+              <h1
+                onClick={() => {
+                  setFilters({
+                    value: [0, 100000000],
+                    selectedRatings: [],
+                    searchQuery: "",
+                  });
+                  setValue([0, 100000000]);
+                  setSelectedRatings([]);
+                  setSearchQuery("");
+                  setFilterApplied(false);
+                }}
+                className="text-[#533FDB] text-sm cursor-pointer outfitRegular"
+              >
+                Clear All
+              </h1>
+            </div>
+          </div>
+          <div className="relative text-[#1C1C1C66]">
+            <Input
+              type="text"
+              placeholder="Search by 'Developer'"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="focus:outline-[#716AEA08] bg-[#F8F8F8] border outfitRegular bordersearch py-5 pl-10 lg:w-[272px]"
+            />
+            <SearchIcon className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 transform text-gray-400 lg:left-3" />
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center space-x-1">
+            <img src={PriceIcon} className="w-5" />
+            <h1 className="text-[#1C1C1C] text-lg outfitMedium">Price</h1>
+          </div>
+          <div className="mt-2 w-auto max-w-[250px]">
+            <div className="relative">
+              <PriceRangeSlider
+                getAriaLabel={() => "Property Price Range"}
+                value={value}
+                min={0}
+                max={100000000}
+                step={0.1}
+                className="ml-2"
+                onChange={handleChange}
+                getAriaValueText={valuetext}
+              />
+            </div>
+            <div className="flex items-center outfitRegular justify-between text-sm text-neutral-600">
+              <div className="">₹ {formatPrice(value[0])}</div>
+              <div className="">₹ {formatPrice(value[1])}</div>
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex items-center space-x-1">
+            <img src={RatingIcon} className="w-5" />
+            <h1 className="text-[#1C1C1C] text-lg outfitMedium">Rating</h1>
+          </div>
+          <div>
+            {[
+              { value: 4.5, checked: selectedRatings.includes(4.5) },
+              { value: 4, checked: selectedRatings.includes(4) },
+              { value: 3.5, checked: selectedRatings.includes(3.5) },
+              { value: 3, checked: selectedRatings.includes(3) },
+            ].map((rating) => (
+              <Rating
+                key={rating.value}
+                value={rating.value}
+                checked={rating.checked}
+                onChange={handleRatingChange}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="">
+        {filtersChanged && (
+          <div className="">
+            <button
+              onClick={applyFilters}
+              className="bg-[#533FDB] text-white py-2 px-[116px] rounded"
+            >
+              Apply
+            </button>
+          </div>
+        )}
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default FilterCard;
